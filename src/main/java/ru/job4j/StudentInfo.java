3ba3214e5d0cae7f1712da5f8519f4cebce31813
Daniil <daniil252000@mail.ru>
2022-04-11 09:04:23 +0400

    4.1. Разбор первой программы. [#159033] 1

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+     public static void main(String[] args){
+         System.out.println("Daniil Seredin");
+         System.out.println("24.01.2000");
+     }
+}
