b9b2a7646744af5487b196a9c644b644178c850e
Daniil <daniil252000@mail.ru>
2022-04-11 14:26:30 +0400

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,6 @@
 package ru.job4j;
 
 public class StudentInfo {
-     @SuppressWarnings("checkstyle:WhitespaceAround")
      public static void main(String[] args) {
          System.out.println("Daniil Seredin");
          System.out.println("24.01.2000");
