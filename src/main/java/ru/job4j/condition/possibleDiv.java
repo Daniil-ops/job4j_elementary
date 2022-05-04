9f9f5c257c7c8ab23d1bafd6b4080a827185b697
Daniil <daniil252000@mail.ru>
2022-05-04 13:47:27 +0400

    4.1. Операторы ветвлений [#3611]

---
+++ src/main/java/ru/job4j/condition/possibleDiv.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        posibleDiv(4);
+        posibleDiv(0);
+        posibleDiv(-4);
+    }
+
+    public static void posibleDiv(int number) {
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
+
+        if (number < 0) {
+            System.out.println("This is negative number");
+        }
+    }
+}
