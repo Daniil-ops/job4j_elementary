aebc780108e055aa16bfea48a48ce56e563730e3
Daniil <daniil252000@mail.ru>
2022-05-24 10:07:50 +0400

    5.5. Простое число [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = false;
+        for (int i = 2; i <= number; i++) {
+            prime = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        return prime;
+    }
+}
