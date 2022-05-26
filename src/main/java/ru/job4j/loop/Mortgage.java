a897be62761caee56abcfb050deb5f1663cf0f8b
Daniil <daniil252000@mail.ru>
2022-05-26 09:58:10 +0400

    5.7. Ипотека [#156315 #304620]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,11 +2,14 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = -1;
-        double i = salary;
-        while (i >= 0) {
-            i = i + percent / 100 * i - amount;
-            year++;
+        int year = 0;
+        while (salary > 0) {
+            salary = (int) (salary + percent / 100 * salary - amount);
+            if (salary <= 0) {
+                break;
+            } else {
+                year++;
+            }
         }
         return year;
     }
